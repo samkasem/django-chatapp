fc96a12daeaa1b2d3221de6bb4b618c1807a9937
sunhine526 <mr.sunshine526@gmail.com>
2022-10-15 13:58:20 +0300

    final commit
    final commit

---
+++ chatapp/chat/models.py
@@ -0,0 +1,14 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from datetime import datetime
+
+
+# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=500)
+
+class Message(models.Model):
+    value = models.CharField(max_length=1000000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    room = models.CharField(max_length=500)
+    user = models.CharField(max_length=500)
